d2610064b2d95b19e577d2a8121245322c85d4c6
RNovice <89009561+RNovice@users.noreply.github.com>
2022-07-18 16:17:57 +0800

    Update app.js
    新增註解

--- app.js
+++ app.js
@@ -9,32 +9,36 @@ app.set ('view engine', 'handlebars')
 
 app.use(express.static('public'))
 
+//根目錄
 app.get('/', (req, res) => {
-    res.render('index', { restaurants : restaurantList.results })
+  res.render('index', { restaurants : restaurantList.results })
 })
 
+//各餐廳詳細資料
 app.get('/restaurants/:restaurant_id', (req, res) => {
-    const restaurant = restaurantList.results.find( rtr => rtr.id === Number(req.params.restaurant_id))
-    res.render('show',{ restaurant })
+  const restaurant = restaurantList.results.find( rtr => rtr.id === Number(req.params.restaurant_id))
+  res.render('show',{ restaurant })
 })
 
+//搜尋功能
 app.get('/search', (req, res)  => {
-    const keyword = [
-        ...(restaurantList.results.filter( 
-            rtr => rtr.name.toLowerCase().includes(req.query.keyword.toLowerCase()))),
-        ...(restaurantList.results.filter( 
-            rtr => rtr.name_en.toLocaleLowerCase().includes(req.query.keyword.toLowerCase()))),
-        ...(restaurantList.results.filter( 
-            rtr => rtr.category.includes(req.query.keyword)))
-    ]
+  //將與關鍵字相符的資料放入keyword中
+  const keyword = [
+    ...(restaurantList.results.filter( 
+        rtr => rtr.name.toLowerCase().includes(req.query.keyword.toLowerCase()))),
+    ...(restaurantList.results.filter( 
+        rtr => rtr.name_en.toLocaleLowerCase().includes(req.query.keyword.toLowerCase()))),
+    ...(restaurantList.results.filter( 
+        rtr => rtr.category.includes(req.query.keyword)))
+  ]
+  //移除重複的資料
+  const filteredSearchResults = keyword.filter((item,index,arr)=>{
+      return arr.indexOf(item) === index
+  })
 
-    const filteredSearchResults = keyword.filter((item,index,arr)=>{
-        return arr.indexOf(item) === index
-    })
-
-    res.render('index', { restaurants : filteredSearchResults, keyword : req.query.keyword })
+  res.render('index', { restaurants : filteredSearchResults, keyword : req.query.keyword })
 })
 
 app.listen(port, () =>{
-    console.log(`Express is listening on http://localhost:${port}`)
+  console.log(`Express is listening on http://localhost:${port}`)
 })
